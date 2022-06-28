bfbb97a75145fbd936f415ed15e6aceaa42938e7
ekrammmm <85333580+ekrammmm@users.noreply.github.com>
2022-06-28 08:53:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class ReverseExample
+
+{
+
+    public static void Main(string[] args)
+    {
+        char ans = 'Y';
+
+        while (ans == 'Y' || ans == 'y')
+        {
+
+            {
+                int n, reverse = 0, rem;
+
+                Console.WriteLine("Enter a number: ");
+
+
+                n = int.Parse(Console.ReadLine());
+
+                while (n != 0)
+
+                {
+
+                    rem = n % 10;
+
+                    reverse = reverse * 10 + rem;
+
+                    n /= 10;
+
+
+                }
+
+                Console.Write("Reversed Number: " + reverse);
+
+            }
+            Console.WriteLine("\npress y to continue or n to exit\n");
+            ans = char.Parse(Console.ReadLine());
+
+        }
+       
+    }
+}
